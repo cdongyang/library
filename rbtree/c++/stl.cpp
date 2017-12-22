f4eb09b7ff936b9b8c1e2341fd8fe26dd575336d
陈东阳 <chendongyang@sunteng.com>
2017-12-22 13:12:14 +0800

    stl.cpp

--- rbtree/c++/stl.cpp
+++ rbtree/c++/stl.cpp
@@ -12,14 +12,28 @@ int main() {
 	set<int>s;
 	//var benchRand = randint.Rand{First: 23456, Add: 12345, Mod: 1e9 + 7}
 	randint benchRand=randint{23456,12345,int(1e9+7)};
-	randint rand = benchRand;
 	for(int i=100;i<=1e7;i*=10) {
+		randint rand = benchRand;
 		s.clear();
+		clock_t allStart=clock();
+
 		clock_t start = clock();
+		int *arr = new int[int(1e7)];
 		for(int j=0;j<i;j++) {
-			s.insert(rand.Int());
+			arr[i]=rand.Int();
+			s.insert(arr[i]);
 		}
 		clock_t end = clock();
-		printf("insert %d use time %.3f s %.0fns/op\n",i,double(end-start)/CLOCKS_PER_SEC,double(end-start)/CLOCKS_PER_SEC*1e9/i);
+		printf("insert %d elem use time %.3fs %.0fns/op\n",i,double(end-start)/CLOCKS_PER_SEC,double(end-start)/CLOCKS_PER_SEC*1e9/i);
+
+		start=clock();
+		for(int j=0;j<i;j++) {
+			s.erase(arr[i]);
+		}
+		end=clock();
+		printf("erase %d elem use time %.3fs %.0fns/op\n",i,double(end-start)/CLOCKS_PER_SEC,double(end-start)/CLOCKS_PER_SEC*1e9/i);
+
+		clock_t allEnd=clock();
+		printf("insert and erase elem %d use time %.3fs %.0fns/op\n",i,double(allEnd-allStart)/CLOCKS_PER_SEC,double(allEnd-allStart)/CLOCKS_PER_SEC*1e9/i);
 	}
 }
